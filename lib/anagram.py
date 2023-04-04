9fdf2862381c1a43f22f2b888a248b7b369037b7
madeline <madeline.j.dick@gmail.com>
2023-04-04 15:50:11 -0500

    passed test

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = sorted([letter for letter in word])
+
+    def match(self, word_list):
+        self.word_list = word_list
+
+        matches = []
+        
+        for word in word_list:
+            if sorted([letter for letter in word]) == self.word:
+                matches.append(word)
+        return matches
